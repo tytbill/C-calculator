32c899a3d699e856ddf60c84775dcb3882f0832f
TYTBILL <tangtang324425@163.com>
2023-07-24 18:16:12 +0800

    new

--- .vscode/dam.c
+++ .vscode/dam.c
@@ -1,13 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct Node
+{
+    int data;
+    struct Node* next;
+};
+
+typedef struct Node Node;
+
+void insert_node(Node** p_first, Node** p_last, int data)
+{
+    Node* p_node = (Node*)malloc(sizeof(Node));
+    p_node->data = data;
+    p_node->next = NULL;
+
+    if (*p_first == NULL)
+    {
+        *p_first = p_node;
+        *p_last = p_node;
+    }
+    else
+    {
+        (*p_last)->next = p_node;
+        *p_last = p_node;
+    }
+}
+
 int main()
 {
-    int a = 0;
-    int* pA = &a;
-    *pA = 123;
-    int* iptr = (int*)malloc(sizeof(int));
-    *iptr = 123;
-    printf("%d", *iptr);
+    Node* first = NULL;
+    Node* last = NULL;
+    insert_node(&first, &last, 111);
+    insert_node(&first, &last, 222);
+    insert_node(&first, &last, 333);
     return 0;
 }
